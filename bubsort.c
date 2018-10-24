13733eef32b34ad4be8d3cf4954c2d40afec5c96
CECCS17D04 <43601347+CECCS17D04@users.noreply.github.com>
2018-10-24 20:43:26 +0530

    Update bubsort.c

--- bubsort.c
+++ bubsort.c
@@ -10,18 +10,18 @@ void main()
    scanf("%d",&n);
    printf("Enter elements:");
    for(i=0;i<n;i++)
-	    scanf("%d",&a[i]);
+   	scanf("%d",&a[i]);
    for(i=0;i<n;i++)
 	{  for(j=0;j<n;j++)
 		 { if (a[j]>a[j+1])
 			  {  temp=a[j];
 			     a[j]=a[j+1];
-           a[j+1]=temp;			
+         		     a[j+1]=temp;			
 			  }
 		 }
 	}
    printf("Sorted elements:");
    for(i=0;i<n;i++)
-	    printf("%d",a[i]);
+   	printf("%d",a[i]);
    getch();
 }
